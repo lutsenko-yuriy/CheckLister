471a55321dc3d91f2c639f3da25ba079d255a0ba
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2026-09-20 17:29:28 +0200

    refactor(WU4): isolate iOS link handoff module (CheL-36)

--- ios/CheckLister/AppDelegate.swift
+++ ios/CheckLister/AppDelegate.swift
@@ -58,56 +58,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   }
 }
 
-@objc(ExternalRunLinking)
-class ExternalRunLinking: NSObject {
-  private static var isActive = false
-  private static var pendingURLs: [URL] = []
-
-  @objc
-  var methodQueue: DispatchQueue {
-    DispatchQueue.main
-  }
-
-  static func receive(_ urls: [URL]) {
-    dispatchPrecondition(condition: .onQueue(.main))
-    guard isActive else {
-      pendingURLs.append(contentsOf: urls)
-      return
-    }
-
-    urls.forEach(deliver)
-  }
-
-  private static func deliver(_ url: URL) {
-    RCTLinkingManager.application(
-      UIApplication.shared,
-      open: url,
-      options: [:]
-    )
-  }
-
-  @objc(activate:rejecter:)
-  func activate(
-    _ resolve: RCTPromiseResolveBlock,
-    rejecter _: RCTPromiseRejectBlock
-  ) {
-    ExternalRunLinking.isActive = true
-    let urls = ExternalRunLinking.pendingURLs.map(\.absoluteString)
-    ExternalRunLinking.pendingURLs.removeAll()
-    resolve(urls)
-  }
-
-  @objc
-  func deactivate() {
-    ExternalRunLinking.isActive = false
-  }
-
-  @objc
-  static func requiresMainQueueSetup() -> Bool {
-    true
-  }
-}
-
 class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
   override func sourceURL(for bridge: RCTBridge) -> URL? {
     self.bundleURL()
